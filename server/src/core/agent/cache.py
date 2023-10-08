5c940d9bf8b7c52ecf35d2af58db61ff33efcc71
Victor Porton <porton.victor@gmail.com>
2023-10-08 18:04:54 +0300

    a response to code review

--- server/src/core/agent/cache.py
+++ server/src/core/agent/cache.py
@@ -2,20 +2,19 @@
 from core.common import conn
 
 
-# TODO: Why were there used base64 encodings?
-#       Experiments show that it receives binary strings as arguments and works fine.
+# Base64 is used to avoid outputting control symbols, etc. to a terminal during debugging.
 class PredictCache:
     def __init__(self, redis):
         self.redis = redis
 
     async def put(self, key, value: str):
-        # k = encode_to_base64(key)
-        await self.redis.hset("predict_cache:" + key, "value", value)
+        k = encode_to_base64(key)
+        await self.redis.hset("predict_cache:" + k, "value", value)
 
     async def get(self, key):
-        # k = encode_to_base64(key)
-        return await self.redis.hget("predict_cache:" + key, "value")
+        k = encode_to_base64(key)
+        return await self.redis.hget("predict_cache:" + k, "value")
 
     async def exists(self, key) -> bool:
-        # k = encode_to_base64(key)
-        return await self.redis.exists("predict_cache:" + key)
+        k = encode_to_base64(key)
+        return await self.redis.exists("predict_cache:" + k)
