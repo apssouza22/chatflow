92f4491b60f330666ed3d3bcc928278632b03082
Victor Porton <porton.victor@gmail.com>
2023-10-17 23:41:27 +0300

    SQLAlchemy/Alembic tables

--- alembic/env.py
+++ alembic/env.py
@@ -16,9 +16,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from server.src.core.scheme import Base
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- server/src/core/history/history_dao.py
+++ server/src/core/history/history_dao.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from core.common import pg_conn
 
 import sqlalchemy
 
@@ -8,20 +9,23 @@ class HistoryDao:
         self.db = db
 
     def persist_message(self, user_ref, app_key, msg, is_bot_replay):
-        insert_query = sql.SQL(
-            """
-            INSERT INTO chat_messages(user_ref, chatbot_id, message, is_bot_reply)
-            VALUES(%s, %s, %s, %s)
-            """
+        self.db.insert(
+            "chat_messages",
+            user_ref=user_ref,
+            chatbot_id=app_key,
+            message=msg,
+            is_bot_reply=is_bot_replay,
         )
-        self.db.execute(insert_query, (user_ref, app_key, msg, is_bot_replay))
 
     def get_latest_messages(self, user_ref: str, app_key: str) -> List[Dict]:
+        with Session(engine) as session:
+            return session.execute(select(chat_messages).where(chat_messages.chatbot_id == app_key).where(
+                chat_messages.user_ref == user_ref).order_by(chat_messages.c.createdat.desc()).limit(50)).fetchall()
         return self.db.fetch_all(
-            """
-            SELECT * FROM chat_messages where chatbot_id = %s and user_ref = %s
-            ORDER BY createdat DESC
-            LIMIT 50
-            """,
-            (app_key, user_ref),
-        )
+                """
+                SELECT * FROM chat_messages where chatbot_id = %s and user_ref = %s
+                ORDER BY createdat DESC
+                LIMIT 50
+                """,
+                (app_key, user_ref),
+            ).fetchall()
