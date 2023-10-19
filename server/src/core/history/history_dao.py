ec097afbe4cc07ed898a742d269bf9b594b49ee5
Victor Porton <porton.victor@gmail.com>
2023-10-19 22:26:02 +0300

    int instead of str

--- server/src/core/history/history_dao.py
+++ server/src/core/history/history_dao.py
@@ -9,19 +9,19 @@ class HistoryDao:
     def __init__(self, db: DBConnection):
         self.db = db
 
-    def persist_message(self, user_ref, app_key, msg, is_bot_replay):
+    def persist_message(self, user_ref: int, app_key, msg, is_bot_replay):
         insert_query = sql.SQL(
             """
             INSERT INTO chat_messages(user_ref, chatbot_id, message, is_bot_reply)
-            VALUES(%s, %s, %s, %s)
+            VALUES(%d, %s, %s, %s)
             """
         )
         self.db.execute(insert_query, (user_ref, app_key, msg, is_bot_replay))
 
-    def get_latest_messages(self, user_ref: str, app_key: str) -> List[Dict]:
+    def get_latest_messages(self, user_ref: int, app_key: str) -> List[Dict]:
         return self.db.fetch_all(
             """
-            SELECT * FROM chat_messages where chatbot_id = %s and user_ref = %s
+            SELECT * FROM chat_messages where chatbot_id = %s and user_ref = %d
             ORDER BY createdat DESC
             LIMIT 50
             """,
