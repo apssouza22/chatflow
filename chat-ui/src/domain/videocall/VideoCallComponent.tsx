687338fb55938be0a0f09626a74153b2c84a01a7
Alexsandro Souza <alexsandro.souza@workday.com>
2023-12-28 13:11:01 +0100

    External socker server property

--- chat-ui/src/domain/videocall/VideoCallComponent.tsx
+++ chat-ui/src/domain/videocall/VideoCallComponent.tsx
@@ -6,119 +6,122 @@ import {MdVideoCall} from "react-icons/md";
 import {VideoChatApp} from "./index";
 import io from "socket.io-client";
 
-let  videoChatApp: VideoChatApp ;
-export function VideoCallComponent({
-                              chatController,
-                              actionRequest,
-                          }: {
-    chatController: ChatController;
-    actionRequest: ActionRequest;
-}) {
-    const [userList, setUserList] = useState([]);
-    const chatCtl: ChatController = chatController;
-    useEffect(() => {
-        videoChatApp = new VideoChatApp({
-            localVideo: document.getElementById("local-video"),
-            remoteVideo: document.getElementById("remote-video"),
-            // remoteVideo: document.getElementById("remote-audio"), We can use this for audio as well
-            socket: io.connect("localhost:4000"),
-            userList: setUserList
-        });
-        videoChatApp.start();
-    }, []);
+let videoChatApp: VideoChatApp;
 
-    const startVideoCall = useCallback((user): void => {
-        console.log("Starting video call with " + user)
-        videoChatApp.callUser(user)
-    }, [actionRequest, chatCtl]);
+export function VideoCallComponent(
+    {
+      chatController,
+      actionRequest,
+    }: {
+      chatController: ChatController;
+      actionRequest: ActionRequest;
+    }) {
+  const [userList, setUserList] = useState([]);
+  const chatCtl: ChatController = chatController;
+  useEffect(() => {
+    console.log("Connecting to socket server", process.env.REACT_APP_SOCKET_SERVER)
+    videoChatApp = new VideoChatApp({
+      localVideo: document.getElementById("local-video"),
+      remoteVideo: document.getElementById("remote-video"),
+      // remoteVideo: document.getElementById("remote-audio"), We can use this for audio as well
+      socket: io.connect(process.env.REACT_APP_SOCKET_SERVER),
+      userList: setUserList
+    });
+    videoChatApp.start();
+  }, []);
+
+  const startVideoCall = useCallback((user): void => {
+    console.log("Starting video call with " + user)
+    videoChatApp.callUser(user)
+  }, [actionRequest, chatCtl]);
 
 
-    const cancelVideoCall = useCallback((): void => {
-        chatCtl.setActionRequest({
-            type: 'text',
-            placeholder: 'Please enter your text.',
-            always: true,
-        })
-    }, [actionRequest, chatCtl]);
-    return (
+  const cancelVideoCall = useCallback((): void => {
+    chatCtl.setActionRequest({
+      type: 'text',
+      placeholder: 'Please enter your text.',
+      always: true,
+    })
+  }, [actionRequest, chatCtl]);
+  return (
+      <Box
+          padding="0 20px"
+          flex="1"
+      >
         <Box
-            padding="0 20px"
-            flex="1"
+            flex="1 1 auto"
+            display="flex"
+            css={{
+              '& > *': {
+                flex: '1 1 auto',
+                minWidth: '0',
+              },
+              '& > * + *': {
+                marginLeft: '1',
+              },
+            }}
         >
-            <Box
-                flex="1 1 auto"
-                display="flex"
-                css={{
-                    '& > *': {
-                        flex: '1 1 auto',
-                        minWidth: '0',
-                    },
-                    '& > * + *': {
-                        marginLeft: '1',
-                    },
-                }}
-            >
-                <Menu>
-                    <MenuButton>
-                        <Button
-                            type="button"
-                            onClick={startVideoCall}
-                            variant="solid"
-                            colorScheme="green"
-                            leftIcon={<MdVideoCall/>}
-                        >
-                            Call
-                        </Button>
-                    </MenuButton>
-                    <MenuList>
-                        {userList.map((user, index) => (
-                            <MenuItem onClick={() => {
-                                startVideoCall(user)
-                            }}>
-                                {user}
-                            </MenuItem>
-                        ))}
+          <Menu>
+            <MenuButton>
+              <Button
+                  type="button"
+                  onClick={startVideoCall}
+                  variant="solid"
+                  colorScheme="green"
+                  leftIcon={<MdVideoCall/>}
+              >
+                Call
+              </Button>
+            </MenuButton>
+            <MenuList>
+              {userList.map((user, index) => (
+                  <MenuItem onClick={() => {
+                    startVideoCall(user)
+                  }}>
+                    {user}
+                  </MenuItem>
+              ))}
 
-                    </MenuList>
-                </Menu>
-                <Button
-                    type="button"
-                    onClick={cancelVideoCall}
-                    variant="solid"
-                    colorScheme="red"
-                    mr={2}
-                    mb={2}
-                >
-                    Cancel
-                </Button>
+            </MenuList>
+          </Menu>
+          <Button
+              type="button"
+              onClick={cancelVideoCall}
+              variant="solid"
+              colorScheme="red"
+              mr={2}
+              mb={2}
+          >
+            Cancel
+          </Button>
 
 
-            </Box>
-            <Box>
-                <video
-                    autoPlay
-                    style={{
-                        border: '1px solid #cddfe7',
-                        width: '100%',
-                        boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.2)',
-                    }}
-                    id="remote-video"
-                />
-                <video
-                    autoPlay
-                    muted
-                    style={{
-                        border: '1px solid #cddfe7',
-                        width: '300px',
-                        boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.2)',
-                        borderRadius: '5px',
-                        marginTop: '20px', marginBottom: '20px'
-                    }}
-                    id="local-video"
-                />
-                <audio autoPlay id="remote-audio"/>
-            </Box>
         </Box>
-    );
+        <Box>
+          <video
+              autoPlay
+              style={{
+                border: '1px solid #cddfe7',
+                width: '100%',
+                boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.2)',
+              }}
+              id="remote-video"
+          />
+          <video
+              autoPlay
+              muted
+              style={{
+                border: '1px solid #cddfe7',
+                width: '300px',
+                boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.2)',
+                borderRadius: '5px',
+                marginTop: '20px', marginBottom: '20px'
+              }}
+              id="local-video"
+          />
+          <audio autoPlay id="remote-audio"/>
+        </Box>
+      </Box>
+  );
 }
 
