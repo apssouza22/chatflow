56674cf42870e9523806c8c81f0d3d8c79f9d082
Alexsandro Souza <alexsandro.souza@workday.com>
2024-07-09 16:19:32 +0100

    Create task pipeline

--- server/src/core/agency/agent_service.py
+++ server/src/core/agency/agent_service.py
@@ -1,6 +1,5 @@
-from core.agency.agents import DefaultAgent, Task
+from core.agency.agents import Task
 from core.agency.command_agent import CommandAgent
-from core.agency.command_func_agent import CommandFuncAgent
 from core.agency.dock_picker_agent import DocPickerAgent
 from core.agency.json_extractor_agent import JSONExtractorAgent
 from core.agency.long_memory_agent import LongMemoryAgent
@@ -15,34 +14,67 @@ class AgentService:
     def __init__(self, llm: LLMService, doc_service: DocService, chat_memory: ChatMemoryService):
         self.chat_memory = chat_memory
         self.question_answer_agent = QuestionAnswerAgent("question-answer", llm, chat_memory)
-        # self.command_agent = CommandAgent("command", llm)
-        self.command_agent = CommandFuncAgent("command", llm)
         self.doc_service = doc_service
         self.llm = llm
         self.dock_picker_agent = DocPickerAgent("dock-picker", llm)
         self.user_intent_agent = UserIntentAgent("user-intent", llm)
         self.json_extractor_agent = JSONExtractorAgent("json-extractor", llm)
         self.long_term_memory_agent = LongMemoryAgent("long-term-memory", llm, chat_memory, self.json_extractor_agent)
+        self.command_agent = CommandAgent("command", llm, "gpt-3", self.json_extractor_agent)
 
     def run_tasks(self, prompt: str) -> dict:
         username = "user1234"
-        docs = self.doc_service.search_docs("chat|demo", prompt)
-        doc_task = self.dock_picker_agent.process(Task(prompt, docs))
-        doc = self.dock_picker_agent.filter_doc(docs, doc_task.output.message)
-        task = self.user_intent_agent.process(Task(prompt))
-        self.long_term_memory_agent.process(Task(prompt, username))
-        self.chat_memory.update_short_memory(username, {"message": prompt, "role": "user", "context": doc.text})
-        if not self.user_intent_agent.is_action(task):
-            resp = self.question_answer_agent.process(Task(prompt, username))
-            self.chat_memory.update_short_memory(username, {"message": resp.output.message, "role": "assistant"})
-            return resp.output.message
+        pipeline = TaskPipeline(self, prompt, username)
+        pipeline.search_docs()
+        pipeline.update_memory()
+        pipeline.check_user_intent()
+        pipeline.build_ui_resp()
+        pipeline.update_short_memory()
+        return pipeline.get_user_resp()
 
-        resp = self.command_agent.process(Task(prompt, doc.text))
-        self.chat_memory.update_short_memory(username, {"message": resp.output.message, "role": "assistant"})
-        if resp.output.tools:
-            return self.command_agent.handle_response(resp)
-        json_data = self.json_extractor_agent.process(Task(resp.output.message))
-        return json_data.output.message
+
+class TaskPipeline:
+    def __init__(self, agent_service: AgentService, prompt: str, username: str):
+        self.assistant_msg = None
+        self.username = username
+        self.prompt = prompt
+        self.user_resp = None
+        self.is_action = False
+        self.doc = None
+        self.agent_service = agent_service
+
+    def search_docs(self):
+        docs = self.agent_service.doc_service.search_docs("chat|demo", self.prompt)
+        doc_task = self.agent_service.dock_picker_agent.process(Task(self.prompt, docs))
+        self.doc = self.agent_service.dock_picker_agent.filter_doc(docs, doc_task.output.message)
+
+    def update_memory(self):
+        self.agent_service.long_term_memory_agent.process(Task(self.prompt, self.username))
+        self.agent_service.chat_memory.update_short_memory(
+            self.username,
+            {"message": self.prompt, "role": "user", "context": self.doc.text}
+        )
+
+    def check_user_intent(self):
+        task = self.agent_service.user_intent_agent.process(Task(self.prompt))
+        self.is_action = self.agent_service.user_intent_agent.is_action(task)
+
+    def build_ui_resp(self):
+        if not self.is_action:
+            resp = self.agent_service.question_answer_agent.process(Task(self.prompt, self.username))
+            self.user_resp = resp.output.message
+            self.assistant_msg = resp.output.message
+            return
+
+        resp = self.agent_service.command_agent.process(Task(self.prompt, self.doc.text))
+        self.assistant_msg = resp.output.message
+        self.user_resp = self.agent_service.command_agent.handle_response(resp)
+
+    def update_short_memory(self):
+        self.agent_service.chat_memory.update_short_memory(self.username, {"message": self.assistant_msg, "role": "assistant"})
+
+    def get_user_resp(self):
+        return self.user_resp
 
 
 def agent_service_factory(llm: LLMService, doc_service: DocService, chat_memory: ChatMemoryService) -> AgentService:
---
+++ server/src/core/agency/command_prompt_agent.py
@@ -0,0 +1,113 @@
+import json
+import time
+
+from core.agency.agents import AgentBase, Task
+
+
+class CommandPromptAgent(AgentBase):
+    def __init__(self, name, llm_service):
+        super().__init__(name, llm_service)
+        self.system_prompt = """You are a bot helping the user to execute tasks. \n\n
+        If you are unsure and the answer is not explicitly written in the documentation, say 
+        "Sorry, I don't know how to help with that." \n\n
+        Given the following information from the documentation 
+        provide for the user's task using only this information\n\n 
+        """
+
+    def process(self, task: Task) -> Task:
+        prompt = self._get_main_command_prompt(task.context, task.input)
+        infer = self.llm_service.infer_using_basic(prompt)
+        task.set_output(infer)
+        return task
+
+    def handle_response(self, task: Task) -> dict:
+        pass
+
+    def _get_main_command_prompt(self, doc_context: str, sanitized_query: str) -> list[dict]:
+        return [
+            {
+                "role": "system",
+                "content": f'{self.system_prompt}'
+                           f"DOCUMENTATION\n----------------------------\n\n"
+                           f"{doc_context}\n\n"
+            },
+            {
+                "role": "system",
+                "content": f"The current time and date is {time.strftime('%c')}"
+            },
+            {
+                "role": "user",
+                "content": f"Execute the task using only the provided documentation above."
+            },
+            {
+                "role": "user",
+                "content": response_format_instructions
+            },
+            {
+                "role": "user",
+                "content": 'Determine which command to use, and respond using the format specified above'
+            },
+            {
+                "role": "user",
+                "content": f"Under no circumstances should your response deviate from the following JSON FORMAT:  \n{formatted_response_format} \n"
+            },
+            {
+                "role": "user",
+                "content": f'Here is the user\'s input (remember to respond with a markdown code snippet of a json blob with a single action, and NOTHING else):\n\n'
+                           f'User input: {sanitized_query}'
+            },
+        ]
+
+
+response_format = {
+    "thoughts": {
+        "reasoning": "reasoning. Use future tense e.g. 'I will do this'",
+        "speak": "Friendly thoughts summary to say to user. Use future tense e.g. 'I will do this'. Important: Make sure to always translate your reply to the user's language.",
+    },
+    "command": {
+        "name": "api_call|browse_website|js_func",
+        "args": {"arg name": "value"},
+        "request_render": {
+            "field_name_1": {
+                "field_type": "select",
+                "field_options": ["SP", "RJ"]
+            },
+            "field_name_2": {
+                "field_type": "password",
+                "field_options": []
+            }
+        },
+        "response_render": {
+            "render_type": "list",
+            "fields": ["total"]
+        }
+    },
+}
+formatted_response_format = json.dumps(response_format, indent=4)
+response_format_instructions = f"RESPONSE FORMAT INSTRUCTIONS\n----------------------------\n\n" \
+                               f"When responding to me, please output a response in one of three formats:\n\n" \
+                               f"**Option 1:**\n" \
+                               f"Use this if the command is a API call\n" \
+                               f'Markdown code snippet formatted in the following schema:\n\n' \
+                               f'```json\n command: {{\n   ' \
+                               f'"name": "api_call" \\ The command will be an api call\n' \
+                               f'"args": {{"url": "<url>", "method": "<http method>","data_request": "<JSON object>","headers": "<JSON object>"}} \\ The arguments for the api call\n' \
+                               f'"request_render": {{"<field name>":{{"field_type": "<input|checkbox|select|password>", field_options: "<array string>"}} }} \\ Instruction of how to render the request fields\n' \
+                               f'"response_render": {{"render_type": "<list|chart>", fields: "<array string>"}} \\ Instruction of how to render the response\n' \
+                               f'}}\n```' \
+                               f'\n\n**Option 2:**\n' \
+                               f'Use this if the command is a browse website\n' \
+                               f'Markdown code snippet formatted in the following schema:\n\n' \
+                               f'```json\n command:{{\n   ' \
+                               f'"name": "browse_website" \\ The command will be a browse website\n' \
+                               f'"args": "url": "<url>" \\ The arguments for the browse website\n' \
+                               f'"request_render": "field_type": "<input|checkbox|select|password>", field_options: "<array string>" \\ Instruction of how to render the request fields\n' \
+                               f'}}\n```' \
+                               f'\n\n**Option 3:**\n' \
+                               f'Use this if the command is a Javascript function\n' \
+                               f'Markdown code snippet formatted in the following schema:\n\n' \
+                               f'```json\n command:{{\n   ' \
+                               f'"name": "js_func" \\ The command will be a Javascript function\n' \
+                               f'"args": {{"func_name": "<function_name>", "func_code": "<function_code>", "func_params": "<object_params>"}} \\ The javascript function details\n' \
+                               f'}}\n ```' \
+                               f'Notice: All the options will be along with the ``` thoughts:{{ }}```'
